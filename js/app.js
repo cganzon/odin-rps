7864843fb2e64a272297b4dbd429a8b69577b139
cganzon <crganzon@gmail.com>
2022-05-16 18:42:09 -0700

    add comments to the computerPlay function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 // Function that generates a random computer choice from the choices array
 function computerPlay() {
+    // Create an array with the three choices for rock paper scissors
     const choices = ['rock', 'paper', 'scissors'];
+
+    // Generate a random index based on the length of the choices array
+    // In this case, the number will be between 0 and 3 (not including 3)
+    // Therefore, the number will be between 0 and 2 (which is what we want since arrays start from 0)
     let randIndex = Math.floor(Math.random() * choices.length);
+
+    // Use the generated random index to return a random item in the choices array
     return choices[randIndex];
 }
