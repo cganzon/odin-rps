cfeb94fad8aa7b7921bd1bc2e27255ed8ddd1583
cganzon <crganzon@gmail.com>
2022-05-18 21:08:35 -0700

    refactor code to use buttons instead of prompt

--- js/app.js
+++ js/app.js
@@ -1,67 +1,53 @@
+const buttons = document.querySelectorAll(".btn");
+let playerScore = 0;
+let computerScore = 0;
+const winningScore = 5;
+
+buttons.forEach(button => {
+  button.addEventListener("click", e => {
+    let playerSelection =  e.target.getAttribute("data-choice");
+    let computerSelection = computerPlay();
+    console.log(playerSelection, computerSelection)
+    playRound(playerSelection, computerSelection)
+  })
+})
+
 // Function that generates a random computer choice from the choices array
 function computerPlay() {
-  // Create an array with the three choices for rock paper scissors
-  const choices = ["rock", "paper", "scissors"];
-
-  // Generate a random index based on the length of the choices array
-  // In this case, the number will be between 0 and 3 (not including 3)
-  // Therefore, the number will be between 0 and 2 (which is what we want since arrays start from 0)
+  const choices = ["Rock", "Paper", "Scissors"];
   let randIndex = Math.floor(Math.random() * choices.length);
-
-  // Use the generated random index to return a random item in the choices array
   return choices[randIndex];
 }
 
 // Function that plays a single round of rock paper scissors
 function playRound(playerSelection, computerSelection) {
-  // Make playerSelection case insensitive by converting to lowercase
-  let playerChoice = playerSelection.toLowerCase();
-
   // If there is a draw
-  if (playerChoice === computerSelection) {
-    return `Draw! You both chose ${playerChoice}`;
+  if (playerSelection === computerSelection) {
+    console.log(`Draw! You both chose ${playerSelection}`);
   }
   // If the player wins
   else if (
-    (playerChoice === "rock" && computerSelection === "scissors") ||
-    (playerChoice === "scissors" && computerSelection === "paper") ||
-    (playerChoice === "paper" && computerSelection === "rock")
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Rock")
   ) {
-    return `You win! ${playerChoice} beats ${computerSelection}`;
+    playerScore++;
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
   }
   // If the player loses
   else {
-    return `You lose! ${computerSelection} beats ${playerChoice}`;
+    computerScore++
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
   }
+  console.log(`Player: ${playerScore}`)
+  console.log(`Computer: ${computerScore}`);
+  checkWinner(playerScore, computerScore);
 }
 
-// Function that plays 5 rounds of rock paper scissors and determines a winner at the end of the 5 rounds
-function game() {
-  let playerWins = 0;
-  let computerWins = 0;
-  let roundsPlayed = 0;
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
-    let computerSelection = computerPlay();
-    let outcome = playRound(playerSelection, computerSelection);
-    roundsPlayed++;
-    if (outcome.includes("You win")) {
-      playerWins++;
-    } else if (outcome.includes("You lose")) {
-      computerWins++;
-    }
-    console.log(outcome);
-    console.log(`Player ${playerWins}-${computerWins} Computer`);
-  }
-
-  //   Determining the winner after the 5 rounds are played
-  if (playerWins === computerWins) {
-    console.log("The game is a tie!");
-  } else if (playerWins > computerWins) {
-    console.log("You won the game. Good job!");
-  } else {
-    console.log("You lost the game. Better luck next time!");
+function checkWinner(playerScore, computerScore) {
+  if(playerScore === winningScore) {
+    console.log('You won the game!');
+  } else if(computerScore === winningScore) {
+    console.log('You lost the game!')
   }
 }
-
-game();
